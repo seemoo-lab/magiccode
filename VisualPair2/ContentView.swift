ca764960bba154470b3136845d3a177b388c6a01
Leon Böttger <leon.boettger@icloud.com>
2025-08-11 18:51:48 +0200

    Initial Commit

---
+++ VisualPair2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisualPair2
+//
+//  Created by Leon Böttger on 11.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisualPair2/VisualPair2App.swift
@@ -0,0 +1,17 @@
+//
+//  VisualPair2App.swift
+//  VisualPair2
+//
+//  Created by Leon Böttger on 11.08.25.
+//
+
+import SwiftUI
+
+@main
+struct VisualPair2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
